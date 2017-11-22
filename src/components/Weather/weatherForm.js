3394df7e3cc12ed182576f47b2d60662aa8f228b
aryasadeghy <aryasadeghy@gmail.com>
2017-11-22 21:11:10 +0330

    fix class Name weather

--- src/components/Weather/weatherForm.js
+++ src/components/Weather/weatherForm.js
@@ -19,11 +19,11 @@ export default class WeatherForm extends Component {
         return (
             <div className='formcontianer'> 
                  <form onSubmit={this.handleSubmit} >
-                      <div class="form-group">
+                      <div className="form-group">
                         <input type="text" className='form-control mx-sm-2' ref="location"/>
                         <small id="searchhelp" className="form-text text-muted">Search the city you are looking for</small>
                       </div>
-                     <button type="submit" class="btn btn-outline-primary btn-lg">Search</button>
+                     <button type="submit" className="btn btn-outline-primary btn-lg">Search</button>
                 </form>
             </div> 
         );
