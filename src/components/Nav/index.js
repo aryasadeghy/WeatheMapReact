9d7a523152a74b2c24b65e53b106a6b68a568cca
aryasadeghy <aryasadeghy@gmail.com>
2017-11-23 21:36:06 +0330

    nav re tempalte

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,32 +1,46 @@
 
 import React, { Component } from 'react';
 import './App.css';
-import {Link} from 'react-router-dom'
+import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink ,Input} from 'reactstrap';
 
-const Nav = (props) => (
-
-<nav className="navbar navbar-dark bg-primary navbar-expand-md py-md-2">
-    <a className="navbar-brand" href="#">Bootstrap 4</a>
-    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="navbar-collapse collapse" id="navbarNav">
-        <ul className="navbar-nav">
-        <li className="nav-item py-md-2">
-            <Link to="/">Home</Link>
-            </li>
-            <li className="nav-item py-md-2">
-            <Link to="/about">About</Link>
-                        </li>
-            
-            <li className="nav-item py-md-2">
-            <Link to="/notfound">NotFound</Link>
-                        </li>
-            
-        </ul>
-    </div>
-</nav>
+class Navigation extends Component {
+    constructor(props) {
+        super(props);
+    
+        this.toggle = this.toggle.bind(this);
+        this.state = {
+          isOpen: false
+        };
+      }
+      toggle() {
+        this.setState({
+          isOpen: !this.state.isOpen
+        });
+      }
+render(){
+    return(
+<div>
+<Navbar color="faded" light expand="md">
+  <NavbarBrand href="/">Weather React</NavbarBrand>
+  <NavbarToggler onClick={this.toggle} />
+  <Collapse isOpen={this.state.isOpen} navbar>
+    <Nav className="ml-auto" navbar>
+    <NavItem>
+      <Input type="text" name="email" id="exampleEmail" placeholder="with a placeholder" />
+          </NavItem>
+      <NavItem>
+        <NavLink href="/examples">examples</NavLink>
+      </NavItem>
+      <NavItem>
+        <NavLink href="/about">about</NavLink>
+      </NavItem>
+    </Nav>
+  </Collapse>
+</Navbar>
+</div>
     );
+}
 
+}
 
-export default Nav;
+export default Navigation;
