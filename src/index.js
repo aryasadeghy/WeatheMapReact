b9e911bef6578b9669f53bffa9316eec2f52dd7e
aryasadeghy <aryasadeghy@gmail.com>
2017-11-23 12:48:17 +0330

    addd reactstrap

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 import Routes from './routes';
+import 'bootstrap/dist/css/bootstrap.css';
+
 
 ReactDOM.render(
     <Routes/>
