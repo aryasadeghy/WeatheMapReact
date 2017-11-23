453de9588d4aeb66cbf454da86fee70b89e5a67b
aryasadeghy <aryasadeghy@gmail.com>
2017-11-23 21:31:58 +0330

    add exmaple component

---
+++ src/components/Examples/index.js
@@ -0,0 +1,13 @@
+// src/components/NotFound/index.js
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+const Examples = (props) => (
+      <div className='NotFound'>
+        <h1>
+        <Link to="/?location=qazvin">qazvin</Link>
+
+        </h1>
+      </div>
+    );
+export default Examples
--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,8 @@ import App from './components/App/App';
 import About from './components/About';
 import NotFound from './components/NotFound';
 import Weather from './components/Weather/index';
+import Examples from './components/Examples/index'
+
 
 
 const Routes = (props) => (
@@ -15,6 +17,7 @@ const Routes = (props) => (
     <Route exact path="/" component={Weather}/> 
     <Route path="/about" component={About}/>
     <Route path="/notfound" component={NotFound}/>
+    <Route path="/examples" component={Examples}/>
   </div>
 </Router>
 );
